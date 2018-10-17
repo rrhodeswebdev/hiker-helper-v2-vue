e22346a8b4de074c1496fb2978aa78dff7eb0232
Ryan Rhodes <rrhodes.webdev@gmail.com>
2018-10-17 14:17:37 -0400

    update for deployment

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const serveStatic = require('serve-static')
+const path = require('path')
 
 const app = express()
 
-app.use(serveStatic(__dirname + '/dist'))
+app.use(serveStatic(path.join(__dirname + '/dist')))
 
 const port = process.env.PORT || 5000
 
