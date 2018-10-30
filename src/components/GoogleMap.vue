dabd925fc48f77cc7ec5181d9c926972aeb411fa
Ryan Rhodes <rrhodes.webdev@gmail.com>
2018-10-29 20:16:16 -0700

    added simple marker content

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -5,10 +5,13 @@
       :zoom='10'
       class='map-size'
     >
+      <gmap-info-window :position='infoWindowPos' :opened='infoWindowOpen' :options='infoOptions'>{{ infoWindowContent }}</gmap-info-window>
       <GmapMarker
         v-for="(mark, index) in markers"
         :key='index'
         :position='{lat: mark.location.trailLatitude, lng: mark.location.trailLongitude}'
+        :clickable='true'
+        @click='toggleInfoWindow(mark, index)'
       />
     </GmapMap>
   </div>
@@ -17,7 +20,37 @@
 <script>
 export default {
   name: 'GoogleMap',
-  props: ['locationData', 'markers']
+  props: ['locationData', 'markers'],
+  data() {
+    return {
+      infoWindowPos: null,
+      infoWindowContent: '',
+      infoWindowOpen: false,
+      currentMidx: null,
+      infoOptions: {
+        pixelOffset: {
+          width: 0,
+          height: -35
+        }
+      }
+    }
+  },
+  methods: {
+    toggleInfoWindow(mark, index) {
+      this.infoWindowPos = {
+        lat: mark.location.trailLatitude,
+        lng: mark.location.trailLongitude
+      }
+      this.infoWindowContent = mark.trailName
+
+      if (this.currentMidx == index) {
+        this.infoWindowOpen = !this.infoWindowOpen
+      } else {
+        this.infoWindowOpen = true
+        this.currentMidx = index
+      }
+    }
+  }
 }
 </script>
 
